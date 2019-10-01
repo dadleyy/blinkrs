b4ce50e87788c1ed113242fed2f119b3f6500a2e
Danny Hadley <dadleyy@gmail.com>
2019-10-01 14:35:01 -0400

    correct example

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,13 @@
 //! use blinkrs::{Blinkers, Message};
 //!
 //! fn main() -> Result<(), Box<dyn Error>> {
-//!     let blinkers: Blinkers = Blinkers::new()?;
+//!     let blinkers: Blinkers = match Blinkers::new() {
+//!         Ok(b) => b,
+//!         Err(_e) => {
+//!             println!("unable to find device");
+//!             return Ok(())
+//!         },
+//!     };
 //!     blinkers.send(Message::from("red"))?;
 //!     blinkers.send(Message::from("off"))?;
 //!     Ok(())
