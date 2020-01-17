75957f06bfd1c54b75dc0048671f69b9993e133c
Danny Hadley <dadleyy@gmail.com>
2020-01-17 12:01:19 -0500

    add formatting file (#11)

--- src/error.rs
+++ src/error.rs
@@ -5,27 +5,27 @@ use std::fmt;
 
 #[derive(Debug)]
 pub enum BlinkError {
-    NotFound,
-    DeviceListError(libusb::Error),
+  NotFound,
+  DeviceListError(libusb::Error),
 }
 
 impl From<libusb::Error> for BlinkError {
-    fn from(error: libusb::Error) -> Self {
-        BlinkError::DeviceListError(error)
-    }
+  fn from(error: libusb::Error) -> Self {
+    BlinkError::DeviceListError(error)
+  }
 }
 
 impl fmt::Display for BlinkError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{{ }}")
-    }
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "{{ }}")
+  }
 }
 
 impl Error for BlinkError {
-    fn description(&self) -> &str {
-        match self {
-            BlinkError::NotFound => "not found",
-            BlinkError::DeviceListError(_e) => "unable to find usb device",
-        }
+  fn description(&self) -> &str {
+    match self {
+      BlinkError::NotFound => "not found",
+      BlinkError::DeviceListError(_e) => "unable to find usb device",
     }
+  }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -25,12 +25,12 @@
 //!
 //! [blink(1)]: https://blink1.thingm.com
 
-use libusb::{Context, Device, DeviceHandle, request_type, Direction, RequestType, Recipient};
+use libusb::{request_type, Context, Device, DeviceHandle, Direction, Recipient, RequestType};
 use std::fmt;
 use std::time::Duration;
 
 pub use color::Color;
-use constants::{HID_SET_REPORT, HID_FEATURE, PRODUCT_ID, VENDOR_ID};
+use constants::{HID_FEATURE, HID_SET_REPORT, PRODUCT_ID, VENDOR_ID};
 pub use error::BlinkError;
 pub use message::Message;
 
@@ -40,66 +40,67 @@ mod error;
 mod message;
 
 fn is_blinker(device: &Device) -> bool {
-    if let Ok(desc) = device.device_descriptor() {
-        return desc.num_configurations() > 0 && desc.product_id() == PRODUCT_ID && desc.vendor_id() == VENDOR_ID;
-    }
+  if let Ok(desc) = device.device_descriptor() {
+    return desc.num_configurations() > 0 && desc.product_id() == PRODUCT_ID && desc.vendor_id() == VENDOR_ID;
+  }
 
-    false
+  false
 }
 
 fn send(device: &Device, message: &Message) -> Result<usize, BlinkError> {
-    let config = device.active_config_descriptor()?;
-    let mut handle: DeviceHandle = device.open()?;
-    let interface_num = config.interfaces().nth(0).ok_or(BlinkError::NotFound)?.number();
+  let config = device.active_config_descriptor()?;
+  let mut handle: DeviceHandle = device.open()?;
+  let interface_num = config.interfaces().nth(0).ok_or(BlinkError::NotFound)?.number();
 
-    if let Ok(active) = handle.kernel_driver_active(interface_num) {
-        if active {
-            handle.detach_kernel_driver(interface_num)?;
-        }
+  if let Ok(active) = handle.kernel_driver_active(interface_num) {
+    if active {
+      handle.detach_kernel_driver(interface_num)?;
     }
+  }
 
-    handle.claim_interface(interface_num)?;
-    let buffer = message.buffer();
-    let time = Duration::new(0, 100);
-    let r_type = request_type(Direction::Out, RequestType::Class, Recipient::Interface);
-    let request_value: u16 = HID_FEATURE | (buffer[0] as u16);
-    let out = handle.write_control(r_type, HID_SET_REPORT, request_value, 0x00, &buffer, time);
-    handle.release_interface(interface_num)?;
-    out.map_err(|e| BlinkError::from(e))
+  handle.claim_interface(interface_num)?;
+  let buffer = message.buffer();
+  let time = Duration::new(0, 100);
+  let r_type = request_type(Direction::Out, RequestType::Class, Recipient::Interface);
+  let request_value: u16 = HID_FEATURE | (buffer[0] as u16);
+  let out = handle.write_control(r_type, HID_SET_REPORT, request_value, 0x00, &buffer, time);
+  handle.release_interface(interface_num)?;
+  out.map_err(|e| BlinkError::from(e))
 }
 
 /// Wraps the [`libusb::Context`](https://docs.rs/libusb/0.3.0/libusb/struct.Context.html) type.
 pub struct Blinkers {
-    context: Context,
+  context: Context,
 }
 
 impl fmt::Debug for Blinkers {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Blinkers {{ }}")
-    }
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    write!(f, "Blinkers {{ }}")
+  }
 }
 
 impl Blinkers {
-    fn from_context(ctx: Context) -> Self {
-        Blinkers { context: ctx }
-    }
+  fn from_context(ctx: Context) -> Self {
+    Blinkers { context: ctx }
+  }
 
-    pub fn new() -> Result<Self, BlinkError> {
-        let context: Context = Context::new()?;
-        Ok(Blinkers::from_context(context))
-    }
+  pub fn new() -> Result<Self, BlinkError> {
+    let context: Context = Context::new()?;
+    Ok(Blinkers::from_context(context))
+  }
 
-    pub fn send(&self, cmd: Message) -> Result<usize, BlinkError> {
-        let devices = self.context.devices()?;
-        devices
-            .iter()
-            .filter(is_blinker)
-            .map(|d| send(&d, &cmd))
-            .collect::<Result<Vec<usize>, BlinkError>>().map(|d| d.iter().sum())
-    }
+  pub fn send(&self, cmd: Message) -> Result<usize, BlinkError> {
+    let devices = self.context.devices()?;
+    devices
+      .iter()
+      .filter(is_blinker)
+      .map(|d| send(&d, &cmd))
+      .collect::<Result<Vec<usize>, BlinkError>>()
+      .map(|d| d.iter().sum())
+  }
 
-    pub fn device_count(&self) -> Result<usize, BlinkError> {
-        let devices = self.context.devices()?;
-        Ok(devices.iter().filter(is_blinker).count())
-    }
+  pub fn device_count(&self) -> Result<usize, BlinkError> {
+    let devices = self.context.devices()?;
+    Ok(devices.iter().filter(is_blinker).count())
+  }
 }
